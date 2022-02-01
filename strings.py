0d795922b51f443fa03de6c7ecd8ed335b7b856c
mr-shan <55774764+mr-shan@users.noreply.github.com>
2022-02-01 19:53:42 +0530

    Added strings file

---
+++ strings.py
@@ -0,0 +1,41 @@
+print("""This is kinda backticks in JS, \
+\
+\
+\
+Here you can split the string
+
+And all the spaces are kept as it is
+
+""")
+
+print("Now this is a raw\t string\n\n")
+print('''Mutlitline string 
+
+with single quotes ' ''')
+
+print(1 + 1)
+
+print(6//2)
+
+print("""
+Line 1: \n\n\n\n
+This is a multiline string
+this is quotes: ""
+
+this is single quotes: ''
+End of line
+""")
+
+print("He's really good\n")
+
+
+string = "This is string"
+print(string[1:15:2])
+
+print(string[25::-1])
+print(string[:7:-1])
+
+print("")
+
+print(" is " in string)
+print("name " + "1")
