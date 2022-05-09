698c64bab0a126267b05347c649add8d6afbe1b4
mr-shan <55774764+mr-shan@users.noreply.github.com>
2022-05-09 07:37:49 +0530

    Updated with lamdas and equivalent of comprehension.

--- filter_map_reduce.py
+++ filter_map_reduce.py
@@ -15,10 +15,19 @@ print("The sum of numbers: ", reduce(addition, numbers))
 print("Even numbers from numbers:", [num for num in filter(is_even, numbers)])
 print("Square of numbers:", [num for num in map(get_square, numbers)])
 
-
-print("Using Lambda")
 # Use of lambdas:
-print("The sum of numbers: ", reduce(lambda x, y: x + y, numbers))
+print("\nUsing Lambda")
 print("The sum of numbers: ", reduce(lambda x, y: x + y, numbers))
 print("Even numbers from numbers:", [num for num in filter(lambda x: x % 2 == 0, numbers)])
 print("Square of numbers:", [num for num in map(lambda x: x ** 2, numbers)])
+
+
+# Use of comprehension and generators
+print("\nUsing comprehension and generators")
+sum = 0
+
+print("The sum of numbers: ", [num for num in numbers])
+print("Even numbers from numbers:", [x for x in numbers if x % 2 == 0])
+print("Square of numbers:", [x ** 2 for x in numbers])
+
+print(list(map(lambda x: x ** 2, range(1, 11))))
